6a3c2e9f922634e8cc20b3ef5adbda880a536f30
Traian Pricop <vb6ro.cod@gmail.com>
2020-03-10 17:35:53 +0200

    fix my?stream

--- servere/mystream.php
+++ servere/mystream.php
@@ -20,112 +20,49 @@
 */
 $filelink = "https://embed.mystream.to/pufpln9x8ejh";
 $filelink = "https://embed.mystream.to/edqddehi2hrh";
-function vv($a157,$a158) {
-  $a159 = array();
-  $a160 = 0x0;
-  $a161="";
-  $a162 = '';
-  for($a163 = 0x0; $a163 < 0x100; $a163++) {
-    $a159[$a163] = $a163;
-  }
-  for($a163 = 0x0; $a163 < 0x100; $a163++) {
-    $a160 = ($a160 + $a159[$a163] + ord($a157[$a163 % strlen($a157)])) % 0x100;
-    $a161 = $a159[$a163];
-    $a159[$a163] = $a159[$a160];
-    $a159[$a160] = $a161;
-  }
-  $a163 = 0x0;
-  $a160 = 0x0;
-  for($a191 = 0x0; $a191 < strlen($a158); $a191++) {
-    $a163 = ($a163 + 0x1) % 0x100;
-    $a160 = ($a160 + $a159[$a163]) % 0x100;
-    $a161 = $a159[$a163];
-    $a159[$a163] = $a159[$a160];
-    $a159[$a160] = $a161;
-    $a162 .= chr(ord($a158[$a191]) ^ $a159[($a159[$a163] + $a159[$a160]) % 0x100]);
-  }
-  return $a162;
-}
-
-
+$filelink="https://embed.mystream.to/j7mt4olun0cr";
 if (strpos($filelink,"mystream.") !== false || strpos($filelink,"mstream.") !==false) {
-  require_once('AADecoder.php');
-  include ("obfJS.php");
-  include ("jj.php");
-  $k=0;
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $filelink);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; rv:65.0) Gecko/20100101 Firefox/65.0");
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION  ,1);
-  curl_setopt($ch, CURLOPT_ENCODING, "");
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 25);
-  while ($k<5) {
-   $h = curl_exec($ch);
-   if (preg_match("/Video not found/",$h)) break;
-   $h=decode_code($h);
-   $h1=AADecoder::decode($h);
-   $pat="/setAttribute\(\'src\', *\'(http.+?mp4)\'\)/";
-   if (preg_match($pat,$h1)) break;
-   $pat1="/setAttribute\(\'src\',\s*vv\(key,\s*atob\(\'(\S+)\'/";
-   if (preg_match($pat1,$h1)) break;
-   sleep(1);
-   $k++;
-  }
-  curl_close($ch);
-  if (strpos($h1,"Video not found") === false) {
-  if (preg_match('/([http|https][\.\d\w\-\.\/\\\:\?\&\#\%\_\,]*(\.(srt|vtt)))/', $h1, $s))
+ $h=file_get_contents($filelink);
+ if (preg_match("@(\\$\=\~\[\].*?)\<script@si",$h,$u)) {
+  if (preg_match('/([http|https][\.\d\w\-\.\/\\\:\?\&\#\%\_\,]*(\.(srt|vtt)))/', $h, $s)) // not sure
   $srt=$s[1];
-  $pat="/setAttribute\(\'src\', *\'(http.+?mp4)\'\)/";
-  if (preg_match($pat, $h1, $m)) {
-    $link=$m[1];
-    if (strpos($link,"http") === false && $link) $link="https:".$link;
-  } else {
-    $pat1="/setAttribute\(\'src\',\s*vv\(key,\s*atob\(\'(\S+)\'/";
-    if (preg_match($pat1, $h1,$q))
-     $encoded=$q[1];
-    else
-     $encoded="";
-    $hash="";
-    $t1=explode("<script>",$h1);
-    $t2=explode("</script",$t1[1]);
-    if ($z1 = jjdecode($t2[0])) {
-     $z1=preg_replace_callback(
-      "/atob\(\'(.*?)\'\)/ms",
-      function ($matches) {
-       return base64_decode($matches[1]);
-      },
-      $z1
-     );
-    preg_match("/(\w{20,})/",$z1,$p);
-    $hash=$p[1];
-   }
-   $t2=explode("</script",$t1[2]);
-   $enc=$t2[0];
-   $dec=obfJS();
-   $h=$l=$n="";
-   if(preg_match("/var\s*h\=\'(\w+)\'/ms",$dec,$m))
-    $h=$m[1];
-   if (preg_match("/var\s*l\=\'(\w+)\'/ms",$dec,$m))
-    $l=$m[1];
-   if (preg_match("/var\s*n\=\'(\w+)\'/ms",$dec,$m))
-    $n=$m[1];
-   $test=array();
-   for($j = 0x0; $j < strlen($l); $j++) {
-     for($k = 0x0; $k < strlen($n); $k++) {
-       $test[$l[$j].$n[$k]] = $h[$j + $k];
-     }
-   }
-   $key="";
-   for($i = 0x0; $i < strlen($hash); $i += 0x2) {
-      $key = $key.$test[$hash[$i].$hash[$i + 1]];
-   }
-   $link=vv($key,base64_decode($encoded));
-   if (strpos($link,"http") === false && $link) $link="https:".$link;
+  $code=$u[0];
+  $t1=explode(";",$code);
+  $js=$code;
+  $t3=substr($t1[1],3);
+  $c=explode(",",$t3);
+  $x="0,f,1,a,2,b,d,3,e,4,5,c,6,7,8,9";
+  $y=explode(",",$x);
+  $map=array();
+  for ($k=0;$k<count($c);$k++) {
+    $a1=explode(":",$c[$k]);
+    $map[$y[$k]]="$.".$a1[0];
   }
-  } else
+  $map['o']="$._$";
+  $map['u']="$._";
+  $map['t']="$.__";
+  function cmp($a, $b) {
+    if (strlen($a) == strlen($b)) {
+        return 0;
+    }
+    return (strlen($a) > strlen($b)) ? -1 : 1;
+  }
+  uasort($map, 'cmp');  // sort map strlen
+  foreach($map as $key=>$value) {
+    $js=str_replace($value,$key,$js);
+  }
+  $js=str_replace("+","",$js);
+  $js=str_replace('"','',$js);
+  $js=str_replace('(![])[2]','l',$js);
+  $js = preg_replace_callback('@\\\\(\d{2,3})@', function($c){return chr(base_convert($c[1], 8, 10)); }, $js);
+  $js=str_replace("\\","",$js);
+  if (preg_match('/([http|https][\.\d\w\-\.\/\\\:\?\&\#\%\_\,]*(\.(srt|vtt)))/', $js, $s))   // not sure
+  $srt=$s[1];
+  if (preg_match("/http.+\.mp4/",$js,$m))
+    $link=$m[0];
+  else
+    $link="";
+ } else
     $link="";
 }
 echo $link;
