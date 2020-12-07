3a3d64c9c1a048e347fb0e2ec66411ca8cffe2c3
Traian Pricop <vb6ro.cod@gmail.com>
2020-12-07 13:08:13 +0200

    fix

--- servere/evoload.php
+++ servere/evoload.php
@@ -20,8 +20,13 @@
  */
 
 $filelink="https://evoload.io/e/wEZkuDhnkURe5j";
+$filelink="https://evoload.io/e/imHpGr37G3lq8n";
 if (strpos($filelink,"evoload.io") !== false) {
   $filelink=str_replace("/f/","/e/",$filelink);
+  if (preg_match("/\/e\/(\w+)/",$filelink,$m))
+   $code=$m[1];
+  else
+   $code="";
   include ("rec.php");
   $cookie= __DIR__ ."\evoload.dat";
   $ua = $_SERVER['HTTP_USER_AGENT'];
@@ -30,6 +35,7 @@ if (strpos($filelink,"evoload.io") !== false) {
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_USERAGENT, $ua);
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION  ,1);
+  curl_setopt($ch, CURLOPT_REFERER,"https://evoload.io");
   curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
   curl_setopt($ch, CURLOPT_COOKIEJAR,$cookie);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -38,19 +44,13 @@ if (strpos($filelink,"evoload.io") !== false) {
   $h = curl_exec($ch);
   if (preg_match('/(\/\/[\.\d\w\-\.\/\\\:\?\&\#\%\_\,]*(\.(srt|vtt)))/', $h, $s))
     $srt="https:".$s[1];
-  if (preg_match("/_csrf\:/",$h)) {
-  $t1=explode("code:'",$h);
-  $t2=explode("'",$t1[1]);
-  $code=$t2[0];
-  $t1=explode("_csrf:'",$h);
-  $t2=explode("'",$t1[1]);
-  $csrf=$t2[0];
+
   $key="6Ldv2fYUAAAAALstHex35R1aDDYakYO85jt0ot-c";
   $co="aHR0cHM6Ly9ldm9sb2FkLmlvOjQ0Mw..";
   $loc="https://evoload.io";
   $token=rec($key,$co,"",$loc);
-  $l="https://evoload.io/EvoSecure";
-  $post='{"token":"'.$token.'","code":"'.$code.'","_csrf":"'.$csrf.'"}';
+  $l="https://evoload.io/SecurePlayer";
+  $post='{"code":"'.$code.'","token":"'.$token.'"}';
   $xsrf="";
   $head=array('Accept: application/json, text/plain, */*',
    'Accept-Language: ro-RO,ro;q=0.8,en-US;q=0.6,en-GB;q=0.4,en;q=0.2',
@@ -68,11 +68,17 @@ if (strpos($filelink,"evoload.io") !== false) {
   $h = curl_exec($ch);
   curl_close($ch);
   $r=json_decode($h,1);
-  if (isset($r['src']))
-   $link=$r['src'];
-  } else {
-   $link="";
+  if (isset ($r['subtitles'])) {
+    $srt = $r['subtitles'][0]['system_name'];
   }
+  if (isset($r['stream'])) {
+   if (isset($r['stream']['backup']))
+    $link=$r['stream']['backup'];
+   elseif (isset($r['stream']['src']))
+    $link=$r['stream']['src'];
+  }
+} else {
+   $link="";
 }
 echo $link;
 ?>
