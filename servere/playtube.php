e7876a468be54b8f37cfe917cbe8df6d08d9d5ea
Traian Pricop <vb6ro.cod@gmail.com>
2021-03-15 22:06:27 +0200

    add

---
+++ servere/playtube.php
@@ -0,0 +1,50 @@
+<?php
+ /* resolve playtube
+ * Copyright (c) 2019 vb6rocod
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * examples of usage :
+ * $filelink = input file
+ * $link --> video_link
+ */
+
+$filelink="https://playtube.ws/embed-bfv0sg6wivjr/tt14039028.mp4.html?c1_file=https://seriale-online.net/subtitrarifilme/tt14039028.vtt&c1_label=Romana";
+if (strpos($filelink,"playtube.") !== false) {
+   require_once("JavaScriptUnpacker.php");
+   $ua="Mozilla/5.0 (Windows NT 10.0; rv:81.0) Gecko/20100101 Firefox/81.0";
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, $filelink);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($ch, CURLOPT_USERAGENT, $ua);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION  ,1);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+   curl_setopt($ch, CURLOPT_TIMEOUT, 25);
+   $h = curl_exec($ch);
+   curl_close($ch);
+  $out="";
+  if (preg_match("/eval\(function\(p,a,c,k,e,[r|d]?/",$h)) {
+  $jsu = new JavaScriptUnpacker();
+  $out = $jsu->Unpack($h);
+  }
+  if (preg_match('/(\/\/[\.\d\w\-\.\/\\\:\?\&\#\%\_\,]*(\.(srt|vtt)))/', $h.$out, $s))
+    $srt="https:".$s[1];
+  if (preg_match('/(file|src)\:\s*\"([http|https][\.\d\w\-\.\/\\\:\?\&\#\%\_\,]*(\.(mp4|m3u8)))/', $out.$h, $m))
+  $link=trim($m[2]);
+  if ($link && $flash <> "flash")
+   $link=$link."|Referer=".urlencode("https://playtube.ws");
+}
+echo $link;
+?>
