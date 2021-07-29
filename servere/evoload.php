40fbe139494e989684245721485c1161b8652a85
Traian Pricop <vb6ro.cod@gmail.com>
2021-07-29 13:27:16 +0300

    fix

--- servere/evoload.php
+++ servere/evoload.php
@@ -27,55 +27,83 @@ if (strpos($filelink,"evoload.io") !== false) {
    $code=$m[1];
   else
    $code="";
-  include ("rec.php");
-  $cookie= __DIR__ ."\evoload.dat";
-  $ua = $_SERVER['HTTP_USER_AGENT'];
+  $ua="Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0";
+
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $filelink);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_USERAGENT, $ua);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION  ,1);
   curl_setopt($ch, CURLOPT_REFERER,"https://evoload.io");
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION  ,1);
   curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
   curl_setopt($ch, CURLOPT_COOKIEJAR,$cookie);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 25);
   $h = curl_exec($ch);
+  curl_close($ch);
   if (preg_match('/(\/\/[\.\d\w\-\.\/\\\:\?\&\#\%\_\,]*(\.(srt|vtt)))/', $h, $s))
-    $srt="https:".$s[1];
-
-  $key="6Ldv2fYUAAAAALstHex35R1aDDYakYO85jt0ot-c";
-  $co="aHR0cHM6Ly9ldm9sb2FkLmlvOjQ0Mw..";
-  $loc="https://evoload.io";
-  $token=rec($key,$co,"",$loc);
-  $l="https://evoload.io/SecurePlayer";
-  $post='{"code":"'.$code.'","token":"'.$token.'"}';
-  $xsrf="";
-  $head=array('Accept: application/json, text/plain, */*',
+  $srt="https:".$s[1];
+  if (preg_match("/captcha\_pass\"\s+value\=\"([^\"]+)\"/",$h,$m)) {
+   $pass=$m[1];
+   $l="https://csrv.evosrv.com/captcha";
+   $head=array('Accept: application/json, text/plain, */*',
+   'Accept-Language: ro-RO,ro;q=0.8,en-US;q=0.6,en-GB;q=0.4,en;q=0.2',
+   'Accept-Encoding: deflate',
+   'Origin: https://evoload.io',
+   'Connection: keep-alive',
+   'Referer: https://evoload.io/');
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, $l);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($ch, CURLOPT_USERAGENT, $ua);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $head);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+   curl_setopt($ch, CURLOPT_TIMEOUT, 25);
+   $csrv_token = curl_exec($ch);
+   curl_close($ch);
+   $a=array(
+   'code' => $code,
+   'token' => 'ok',
+   'csrv_token' => $csrv_token,
+   'pass' => $pass,
+   "reff" => '');
+   $post=json_encode($a);
+   $l="https://evoload.io/SecurePlayer";
+   $head=array('Accept: application/json, text/plain, */*',
    'Accept-Language: ro-RO,ro;q=0.8,en-US;q=0.6,en-GB;q=0.4,en;q=0.2',
    'Accept-Encoding: deflate',
    'Content-Type: application/json;charset=utf-8',
-   'X-XSRF-TOKEN: '.$xsrf.'',
-   'Content-Length: '.strlen($post).'',
+   'Content-Length: '.strlen($post),
    'Origin: https://evoload.io',
    'Connection: keep-alive',
    'Referer: '.$filelink);
-  curl_setopt($ch, CURLOPT_URL, $l);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $head);
-  curl_setopt ($ch, CURLOPT_POST, 1);
-  curl_setopt ($ch, CURLOPT_POSTFIELDS, $post);
-  $h = curl_exec($ch);
-  curl_close($ch);
-  $r=json_decode($h,1);
-  if (isset ($r['subtitles'])) {
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, $l);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($ch, CURLOPT_USERAGENT, $ua);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $head);
+   curl_setopt($ch, CURLOPT_POST,1);
+   curl_setopt($ch, CURLOPT_POSTFIELDS,$post);
+   curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
+   curl_setopt($ch, CURLOPT_COOKIEJAR,$cookie);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+   curl_setopt($ch, CURLOPT_TIMEOUT, 25);
+   $h = curl_exec($ch);
+   curl_close($ch);
+   $r=json_decode($h,1);
+
+   if (isset ($r['subtitles'])) {
     $srt = $r['subtitles'][0]['system_name'];
-  }
-  if (isset($r['stream'])) {
-   if (isset($r['stream']['backup']))
-    $link=$r['stream']['backup'];
-   elseif (isset($r['stream']['src']))
-    $link=$r['stream']['src'];
+   }
+   if (isset($r['stream'])) {
+    if (isset($r['stream']['backup']))
+     $link=$r['stream']['backup'];
+    elseif (isset($r['stream']['src']))
+     $link=$r['stream']['src'];
+   }
   }
 } else {
    $link="";
