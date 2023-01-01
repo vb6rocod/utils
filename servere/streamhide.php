bfc1c4ce97527bedf6c5989ebbab773750867883
vb6rocod <vb6ro.cod@gmail.com>
2023-01-01 12:18:44 +0200

    add new

---
+++ servere/streamhide.php
@@ -0,0 +1,42 @@
+<?php
+ /* resolve streamhide
+ * Copyright (c) 2019 vb6rocod
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * examples of usage :
+ * $filelink = input file
+ * $link --> video_link
+ */
+
+$filelink="https://streamhide.to/w/s0rd5nkp8usf";
+if (strpos($filelink,"streamhide") !== false) {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $filelink);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0');
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION  ,1);
+  curl_setopt($ch, CURLOPT_ENCODING,"");
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+  $h = curl_exec($ch);
+  curl_close($ch);
+  if (preg_match('/sources\:\s+\[\{file\:\"([^\"]+)\"/', $h, $m))
+   $link=$m[1];
+  else
+   $link="";
+}
+echo $link;
+// Note: add Referer and Origin to header to play.
+?>
