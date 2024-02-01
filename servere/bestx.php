502e6e01cf010da90d3861fe2c012f4192821b10
vb6rocod <vb6ro.cod@gmail.com>
2024-02-01 12:38:06 +0200

    fix

--- servere/bestx.php
+++ servere/bestx.php
@@ -20,6 +20,7 @@
  */
 
 $filelink="https://bestx.stream/v/D6Rj6gBwm42V/";
+$filelink="https://moviesapi.club/movie/958196";
 /* origin https://w1.moviesapi.club */
 
     function def($d, $e, $f) {
@@ -87,20 +88,48 @@ $filelink="https://bestx.stream/v/D6Rj6gBwm42V/";
   $enc=$m[1];
   require_once("cryptoJsAesDecrypt.php");
   $js=new cryptoJsAesDecrypt();
+  $out="";
   if (preg_match("/eval\(_0x[a-z0-9]+\(([^\)]+)\)/",$h,$n)) {
     $x=trim(str_replace('"',"",$n[1]));
     $z=explode(",",$x);
+
     $mm=player($z[0],$z[1],$z[2],$z[3],$z[4],$z[5]);
     if (preg_match("/\'([^\']+)\'/",$mm,$m))
       $pass=$m[1];
     else
       $pass="";
-  }
   $out = $js->decrypt1($pass,$enc);
+  } elseif (preg_match("/\}\}\}\(_0x\w+,([^\)]+)\)\,/",$h,$m)) {
+  $a="\$b=".$m[1].";";
+  eval ($a);
+  preg_match("/var _0x\w+\=\[([^\]]+)\]/",$h,$m);
+  $a="\$c=array(".$m[1].");";
+  eval ($a);
+    for ($k = 0; $k < $b; $k++) {
+      array_push($c, array_shift($c));
+    }
+  preg_match("/\'\w+\':_0x\w+\((\w+)\)\+_0x\w+\((\w+)\)/",$h,$m);
+  $cc=count($c);
+  for ($k=0;$k<$cc;$k++) {
+    if (strlen($c[($m[1]+$k) % $cc].$c[($m[2]+$k) % $cc])==15)
+    $p[]=$c[($m[1]+$k) % $cc].$c[($m[2]+$k) % $cc];
+  }
+  $out="";
+  for ($k=0;$k<count($p);$k++) {
+  $pass=$p[$k];
+
+  $out .= $js->decrypt1($pass,$enc);
+  }
+  }
+
   $link="";
+  $srt="";
+  $srt1=array();
   if (preg_match("/sources\:\s*\[\{\"file\"\:\"([^\"]+)\"/",$out,$m))
    $link=$m[1];
- }
+  $ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0";
+  $link .="|Referer=".urlencode($host)."&Origin=".urlencode($host)."&User-Agent=".urlencode($ua);
+  }
 
 echo $link;
 ?>
