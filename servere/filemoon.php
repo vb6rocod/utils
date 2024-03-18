10ca7bf3b2469a1cd5b07bc96084436769bbea3b
vb6rocod <vb6ro.cod@gmail.com>
2024-03-18 16:11:09 +0200

    clean

--- servere/filemoon.php
+++ servere/filemoon.php
@@ -21,11 +21,14 @@
 
 $filelink="https://filemoon.sx/e/re09uiwgwgve?c1_file=https://seriale-online.net/subtitrarifilme/tt11291274.vtt&c1_label=Romana";
 // https://filemoon.to/e/dk86vk0iouf2?sub.info=https://fmovies.to/ajax/episode/subtitles/0b5d2788859f1833c7f39eb5f5b02122?&autostart=true
-if (strpos($filelink,"filemoon.") !== false) {
+$filelink="https://furher.in/e/tndogrdpl8p0";
+if (preg_match("/filemoon\.|moonmov\.pro|furher\.|truepoweroflove\.|kerapoxy\./",$filelink)) {
+   $ua="Mozilla/5.0 (Windows NT 10.0; rv:81.0) Gecko/20100101 Firefox/81.0";
+   $t1=explode("?",$filelink);
+   $host="https://".parse_url($t1[0])['host'];
   if (preg_match('/(\/\/[\.\d\w\-\.\/\\\:\?\&\#\%\_\,]*(\.(srt|vtt)))/', $filelink, $s))
     $srt="https:".$s[1];
    require_once("JavaScriptUnpacker.php");
-   require_once ("tear.php");
    $ua="Mozilla/5.0 (Windows NT 10.0; rv:81.0) Gecko/20100101 Firefox/81.0";
    $ch = curl_init();
    curl_setopt($ch, CURLOPT_URL, $filelink);
@@ -39,11 +42,11 @@ if (strpos($filelink,"filemoon.") !== false) {
    curl_setopt($ch, CURLOPT_TIMEOUT, 25);
    $h = curl_exec($ch);
    curl_close($ch);
-
+   //echo $h;
   $out="";
   if (preg_match("/eval\(function\(p,a,c,k,e,[r|d]?/",$h)) {
   $jsu = new JavaScriptUnpacker();
-  preg_match_all("/eval\(function.*?\<\//s",$h,$m);
+  preg_match_all("/eval\(function.*?\<\/script/s",$h,$m);
   for ($k=0;$k<count($m[0]);$k++) {
   $out .= $jsu->Unpack($m[0][$k]);
   }
@@ -51,8 +54,10 @@ if (strpos($filelink,"filemoon.") !== false) {
   $out=$h." ".$out;
   if (preg_match("/sources\:\s*\[\{file\:\"([^\"]+)\"/",$out,$m))
     $link=$m[1];
-   if ($link && $flash <> "flash")
-    $link=$link."|Referer=".urlencode("https://filemoon.sx")."&Origin=".urlencode("https://filemoon.sx");
+  if ($link) {
+    $link=$link."|Referer=".urlencode($host)."&Origin=".urlencode($host);
+    $link .="&User-Agent=".urlencode("Mozilla/5.0 (Windows NT 10.0; rv:81.0) Gecko/20100101 Firefox/81.0");
+  }
 }
 echo $link;
 ?>
