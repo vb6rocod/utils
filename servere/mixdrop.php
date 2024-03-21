15994dd4b2de9b8139d0ce8eeb0003d8a0bbe0eb
vb6rocod <vb6ro.cod@gmail.com>
2024-03-21 12:29:24 +0200

    update

--- servere/mixdrop.php
+++ servere/mixdrop.php
@@ -20,31 +20,32 @@
  */
 
 $filelink="https://mixdrop.co/e/eaeuizxtz0";
-if (strpos($filelink,"mixdrop.co") !== false) {
-  $filelink=str_replace("mixdrop.co/f/","mixdrop.co/e/",$filelink);
+$filelink="https://mixdrop.is/e/4njejwl0cql4w11";
+$mixdrop="/mixdro{0,}p\.|mdy48tn97\.|mdbekjwqa\.|mdfx\w+|mdzsmut|mixdropjmk/";
+if (preg_match($mixdrop,$filelink)) {
+  $filelink=str_replace("/f/","/e/",$filelink);
   require_once("JavaScriptUnpacker.php");
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $filelink);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; rv:65.0) Gecko/20100101 Firefox/65.0");
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION  ,1);
-  curl_setopt($ch,CURLOPT_REFERER,"https://mixdrop.co");
+  curl_setopt($ch,CURLOPT_REFERER,$filelink);
   curl_setopt($ch, CURLOPT_ENCODING, "");
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
   curl_setopt($ch, CURLOPT_TIMEOUT, 15);
   $h3 = curl_exec($ch);
-  if (preg_match("/window\.location/",$h3)) {
-  $t1=explode('window.location = "',$h3);
-  $t2=explode('"',$t1[1]);
-  $l="https://mixdrop.to".$t2[0];
-  curl_setopt($ch, CURLOPT_URL, $l);
-  $h3 = curl_exec($ch);
-  }
   curl_close($ch);
-  $jsu = new JavaScriptUnpacker();
-  $out = $jsu->Unpack($h3);
 
+  $out="";
+  if (preg_match("/eval\(function\(p,a,c,k,e,[r|d]?/",$h3)) {
+  $jsu = new JavaScriptUnpacker();
+  preg_match_all("/eval\(function.*?\<\/script/s",$h3,$m);
+  for ($k=0;$k<count($m[0]);$k++) {
+  $out .= $jsu->Unpack($m[0][$k]);
+  }
+  }
   if (preg_match("/(\/\/[\w|\.\:\?\&\/\=\_\-]+\.mp4\?[\w|\.\:\?\&\/\=\_\-]+)[\'\"]/",$out,$m)) {
       $link="https:".$m[1];
     if (strpos($link,"http") === false) $link="https:".$link;
